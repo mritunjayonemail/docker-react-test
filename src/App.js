811024e644f9cc8d9afaaece3949868a67340419
Kumar, Mritunjay <Mritunjay_Kumar@intuit.com>
2019-10-10 12:27:17 +0530

    staff demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!, I am testing
+          Hi there!, I am testing for staff meeting
         </p>
         <a
           className="App-link"
